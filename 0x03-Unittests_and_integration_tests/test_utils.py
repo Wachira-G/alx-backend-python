cd8592d618e61e4c39f68058db57ea5a1ede0c3d
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-05 23:23:09 +0300

    updete test_utils.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,16 +7,23 @@ from parameterized import parameterized
 
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """paremetized unit test."""
+
     @parameterized.expand(
-            [
-                ({"a": 1}, ["a"], 1),
-                ({"a": {"b": 2}}, ["a"], {"b": 2}),
-                ({"a": {"b": 2}}, ["a", "b",], 2),
-            ]
+        [
+            ({"a": 1}, ["a"], 1),
+            ({"a": {"b": 2}}, ["a"], {"b": 2}),
+            ({"a": {"b": 2}}, ["a", "b"], 2),
+        ]
     )
     def test_access_nested_map(self, input_map, key_path, expected_output):
         """Tests the the mehtod returns what it is supposed to."""
-        self.assertEqual(access_nested_map(input_map, key_path),
-                expected_output)
+        self.assertEqual(
+                access_nested_map(input_map, key_path), expected_output)
+
+    @parameterized.expand([({}, ["a"]), ({"a": 1}, ["a", "b"])])
+    def test_access_nested_map_exception(self, input_map, key_path):
+        """Tests that the method raises an error appropriatesly."""
+        self.assertRaises(KeyError, access_nested_map, input_map, key_path)
