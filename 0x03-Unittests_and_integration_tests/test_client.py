912f93e5a0d2acea05360cc314939f6fa497d71c
Gabriel Wachira <gabbyk.com@gmail.com>
2024-02-06 05:59:16 +0300

    add test client code

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+
+    """clss to test github org class."""
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """Test that org returns the correct value."""
+        expected_url = f"https://api.github.com/orgs/{org_name}"
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        mock_get_json.assert_called_once_with(expected_url)
+        self.assertEqual(result, mock_get_json.return_value)
